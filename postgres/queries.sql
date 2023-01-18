7e4e21e62da6525be017afbc433cc41b10b6cbd0
NikolayDukhman <116606671+NikolayDukhman@users.noreply.github.com>
2023-01-18 14:59:26 +0200

    Update queries.sql

--- postgres/queries.sql
+++ postgres/queries.sql
@@ -1,1 +1,57 @@
--- It's 17.01.23 today. Queries will be added within couple of days
+-- Find top 5 locations by the number of jobs, excluding vacancies with several locations
+
+SELECT
+"dimLocation".name,
+COUNT("listingFacts".location_id) AS job_count
+FROM
+"listingFacts"
+JOIN
+"dimLocation" ON "listingFacts".location_id = "dimLocation".location_id
+WHERE ("dimLocation".name<>' מספר מקומות')
+GROUP BY
+"dimLocation".name
+ORDER BY
+job_count DESC
+LIMIT 5;
+
+
+-- Top 5 categories with most jobs
+SELECT
+"dimCategory".name,
+COUNT("listingFacts".category_id) AS job_count
+FROM
+"listingFacts"
+JOIN
+"dimCategory" ON "listingFacts".category_id = "dimCategory".id
+GROUP BY
+"dimCategory".name
+ORDER BY
+job_count DESC
+LIMIT 5;
+
+-- Bottom 5 categories with least jobs
+SELECT
+"dimCategory".name,
+COUNT("listingFacts".category_id) AS job_count
+FROM
+"listingFacts"
+JOIN
+"dimCategory" ON "listingFacts".category_id = "dimCategory".id
+GROUP BY
+"dimCategory".name
+ORDER BY
+job_count ASC
+LIMIT 5;
+
+
+--Location where the most hi-tech jobs located.
+-- Hi-tech categories: אינטרנט, הייטק-QA, הייטק-חומרה, הייטק-כללי, הייטק-תוכנה, אבטחת מידע 
+SELECT "dimLocation".name, COUNT(*) as count
+FROM "listingFacts"
+JOIN "dimLocation" ON "listingFacts".location_id = "dimLocation".location_id
+JOIN "dimCategory" ON "listingFacts".category_id = "dimCategory".id
+WHERE "dimCategory".name IN ('אינטרנט', 'הייטק-QA', 'הייטק-חומרה', 'הייטק-כללי', 'הייטק-תוכנה', 'אבטחת מידע')
+GROUP BY "dimLocation".name
+ORDER BY count DESC
+LIMIT 5;
+
